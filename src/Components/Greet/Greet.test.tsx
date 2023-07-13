1a45f3f1606b58acdd56ad1b61dfc4418833b81e
Shivankar1999 <sgaur2424@gmail.com>
2023-07-13 11:23:41 +0530

    All Greet test passes and commit

---
+++ src/Components/Greet/Greet.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import Greet from './Greet';
+
+test('Render Hello in Greet', () => {
+  render(<Greet />);
+  const textElement = screen.getByText('Hello');
+  expect(textElement).toBeInTheDocument();
+});
+
+test('Render Greet with props name', () => {
+  render(<Greet name="Shubham" />);
+  const textElement = screen.getByText('Hello Shubham');
+  expect(textElement).toBeInTheDocument();
+});
